501bf037e5042292f9ba9d9e9e496db395557ee5
Bart <salemon876@gmail.com>
2019-02-07 14:03:42 +0100

    Initial commit

---
+++ src/Firm.java
@@ -0,0 +1,31 @@
+class Employer {
+    String name;
+    String surname;
+    int age;
+
+    public void prezentacjaPostaci() {          // i had to name it like that
+        System.out.println(name + " " + surname + " " + age);
+    }
+}
+
+
+public class Firm {
+
+    public static void main(String[] args) {
+        Employer e1 = new Employer();
+        Employer e2 = new Employer();
+        Employer e3 = new Employer();
+        Employer[] emps = {e1, e2, e3};
+        String[] name = {"Piotr", "Lukas", "Mike"};
+        String[] surname = {"Vazowsky", "Witczak", "Crycnyc"};
+        Integer[] age = {26, 25, 30};
+        int dig = 0;
+        for (Employer e : emps) {
+            e.name = name[dig];
+            e.surname = surname[dig];
+            e.age = age[dig];
+            dig++;
+            e.prezentacjaPostaci();
+        }
+    }
+}
