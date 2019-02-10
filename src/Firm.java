29fb23e99beb47d5949784d4abc972fc8fdaff7b
Bart <salemon876@gmail.com>
2019-02-10 21:32:30 +0100

    hello

--- src/Firm.java
+++ src/Firm.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Employer {
     String name;
     String surname;
@@ -11,21 +14,21 @@ class Employer {
 //testing commit
 public class Firm {
 
-    public static void main(String[] args) {
-        Employer e1 = new Employer();
-        Employer e2 = new Employer();
-        Employer e3 = new Employer();
-        Employer[] emps = {e1, e2, e3};
+    public static void main(String[] args){
+        List<Employer> emps = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            emps.add(new Employer());
+        }
         String[] name = {"Piotr", "Lukas", "Mike"};
         String[] surname = {"Vazowsky", "Witczak", "Crycnyc"};
         Integer[] age = {26, 25, 30};
         int dig = 0;
-        for (Employer e : emps) {
-            e.name = name[dig];
-            e.surname = surname[dig];
-            e.age = age[dig];
+        for (Employer emp : emps) {
+            emp.name = name[dig];
+            emp.surname = surname[dig];
+            emp.age = age[dig];
             dig++;
-            e.prezentacjaPostaci();
+            emp.prezentacjaPostaci();
         }
     }
 }
