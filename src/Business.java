4e962aea7b1ae7c9f8033faeac715405d8ed3716
valdife <salemon876@gmail.com>
2019-03-11 19:04:42 +0100

    Create simple switch menu for program

--- src/Business.java
+++ src/Business.java
@@ -1,10 +1,51 @@
-package employer;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
+
+class Human {
+    private String name;
+    private String surname;
+    private Integer age;
+
+    Human(String name, String surname, Integer age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+
+    @Override
+    public String toString(){
+        return getName() + " " + getSurname();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
 
 public class Business {
     private static String corrSurname(String inputSur) {
@@ -12,38 +53,54 @@ public class Business {
     }
 
     public static void main(String[] args) {
-
-        Scanner readFileName = new Scanner(System.in);
-        System.out.println("Enter database name: ");
-        String fName = readFileName.nextLine();
+        Scanner readOption = new Scanner(System.in);
+        System.out.println("(1) Read data from file // (2) End ");
         try {
-            Scanner read = new Scanner(new File("src/employer/" + fName));
-            List<Human> emps = new ArrayList<>();
-            try {
-                while (read.hasNextLine()) {
-                    String[] fileCell = read.nextLine().split(" ");
-                    emps.add(new Human(fileCell[0], fileCell[1], Integer.parseInt(fileCell[2])));
-                }
+            int option = readOption.nextInt();
+            switch (option) {
+                case 1:
+                    Scanner readFileName = new Scanner(System.in);
+                    System.out.println("Enter database name: ");
+                    String fName = readFileName.nextLine();
+                    try {
+                        Scanner read = new Scanner(new File("src/" + fName));
+                        List<Human> emps = new ArrayList<>();
+                        try {
+                            while (read.hasNextLine()) {
+                                String[] fileCell = read.nextLine().split(" ");
+                                emps.add(new Human(fileCell[0], fileCell[1], Integer.parseInt(fileCell[2])));
+                            }
 
-                read = new Scanner(System.in);
-                System.out.println("Enter a surname: ");
-                String inputSur = read.nextLine();
-                String propSur = corrSurname(inputSur);
+                            read = new Scanner(System.in);
+                            System.out.println("Enter a surname: ");
+                            String inputSur = read.nextLine();
+                            String propSur = corrSurname(inputSur);
 
-                boolean found = false;
-                for (Human emp : emps) {
-                    if (propSur.equals(emp.getSurname())) {
-                        System.out.println(emp.toString());;
-                        System.out.println("This person is " + emp.getAge() + " years old.");
-                        found = true;
+                            boolean found = false;
+                            for (Human emp : emps) {
+                                if (propSur.equals(emp.getSurname())) {
+                                    System.out.println(emp.toString());
+                                    System.out.println("This person is " + emp.getAge() + " years old.");
+                                    found = true;
+                                }
+                            }
+                            if (!found) System.out.println("There is no person with this surname in database.");
+                        } catch (NumberFormatException b) {
+                            System.out.println("Error during reading a file. Corrupted?");
+                        }
+                    } catch (FileNotFoundException a) {
+                        System.out.println("Invalid file path.");
                     }
-                }
-                if (!found) System.out.println("There is no person with this surname in database.");
-            } catch (NumberFormatException b) {
-                System.out.println("Error during reading a file. Corrupted?");
+                    break;
+                case 2:
+                    System.out.println("Bye.");
+                    break;
+                default:
+                    System.out.println("There is no option for this digit.");
+                    break;
             }
-        } catch (FileNotFoundException a) {
-            System.out.println("Invalid file path.");
+        } catch (InputMismatchException ex) {
+            System.out.println("Value has to be a digit.");
         }
     }
 }
